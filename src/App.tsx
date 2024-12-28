36640e23f921ccf53ff36709ec84fbc55e176385
Stan <moscarstanley37@gmail.com>
2024-12-28 14:15:52 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="app-title">
+          Cotizador de <span>Cripto Monedas</span>
+        </h1>
+
+        <div className="content"></div>
+      </div>
+    </>
+  )
+}
+
+export default App
