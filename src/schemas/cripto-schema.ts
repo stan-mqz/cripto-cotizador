dafe12ff7a65f1577fd64fe0bfd20e5562185001
Stan <moscarstanley37@gmail.com>
2024-12-28 15:08:10 -0600

    feat: Add axios for API requests and implement crypto fetching logic in store

--- src/schemas/cripto-schema.ts
+++ src/schemas/cripto-schema.ts
@@ -1,7 +1,17 @@
-import {z} from 'zod'
-
+import { z } from "zod";
 
 export const CurrencySchema = z.object({
-    code: z.string(),
-    name: z.string()
+  code: z.string(),
+  name: z.string(),
+});
+
+export const CryptoCurrencyResponseSchema = z.object({
+    CoinInfo: z.object({
+      FullName: z.string(),
+      Name: z.string(),
+    }),
 })
+
+
+export const CryptoCurrenciesResponseSchema = z.array(CryptoCurrencyResponseSchema)
+
--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,20 @@
+import axios from "axios";
 import { create } from "zustand";
+import { CryptoCurrenciesResponseSchema} from "./schemas/cripto-schema";
+
+
+const getCryptos = async () => {
+
+    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
+    const {data : {Data}} = await axios(url)
+    const result = CryptoCurrenciesResponseSchema.safeParse(Data)
+    console.log(result)
+}
 
 export const useCryptoStore = create(() => ({
 
-    
-    
-        fetchCryptos: () => {
-            console.log('Desde Cryptos')
-        }
-    
+  fetchCryptos: () => {
+    getCryptos()
+},
 
-
-}))
+}));
