cdb13ceb5a096f37a2bf5ec8acb9680cbc4feefa
Stan <moscarstanley37@gmail.com>
2024-12-28 14:50:48 -0600

    feat: Add Zustand for state management and implement fetchCryptos in store

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react"
 import { CriptoSearchForm } from "./components/CriptoSearchForm"
+import { useCryptoStore } from "./store"
 
 function App() {
 
+
+  const fetchCryptos = useCryptoStore(state => state.fetchCryptos)
+
+  useEffect(() => {
+    fetchCryptos()
+  }, [])
  
   return (
     <>
---
+++ src/store.ts
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+export const useCryptoStore = create(() => ({
+
+    
+    
+        fetchCryptos: () => {
+            console.log('Desde Cryptos')
+        }
+    
+
+
+}))
