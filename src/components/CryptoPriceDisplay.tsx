115c1c684a5f60f3d4a0fa95ccea7ee800492d07
Stan <moscarstanley37@gmail.com>
2024-12-28 22:24:44 -0600

    feat: Enhance CryptoPriceDisplay with additional price details and styling

--- src/components/CryptoPriceDisplay.tsx
+++ src/components/CryptoPriceDisplay.tsx
@@ -3,18 +3,41 @@ import { useCryptoStore } from "../store";
 
 export const CryptoPriceDisplay = () => {
   const result = useCryptoStore((state) => state.result);
-  const hasResult = useMemo(() => !Object.values(result).includes(""), [result]);
+  const hasResult = useMemo(
+    () => !Object.values(result).includes(""),
+    [result]
+  );
 
   return (
-    <div>
+    <div className="result-wrapper">
       {hasResult && (
         <>
           <h2>Cotización</h2>
           <div className="result">
-            <div className="">
+            <img
+              src={`https://cryptocompare.com/${result.IMAGEURL}`}
+              alt="Imagen cryptomoneda"
+            />
+
+            <div>
               <p>
+                {" "}
                 El precio es de: <span>{result.PRICE}</span>
               </p>
+              <p>
+                Precio más alto del dia: <span>{result.HIGHDAY}</span>
+              </p>
+              <p>
+                Precio más bajo del dia: <span>{result.LOWDAY}</span>
+              </p>
+              <p>
+                Variación últimas 24 horas:{" "}
+                <span>{result.CHANGEPCT24HOUR}</span>
+              </p>
+              <p>
+                {" "}
+                Última actualización: <span>{result.LASTUPDATE}</span>
+                  </p>
             </div>
           </div>
         </>
